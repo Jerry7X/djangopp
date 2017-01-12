7af123a25d3fed06fab05a5049b8d4a8944eaa0b
root <root@localhost.localdomain>
2017-01-12 15:55:49 +0800

    add news and change template

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -4,37 +4,39 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
-from wagtail.wagtailsearch import index
 
+from wagtail.wagtailsearch import index
 # Create your models here.
 
-class BlogIndexPage(Page):
+class NewsDirPage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
 
-class BlogDirPage(Page):
-    intro = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
-
-class BlogPage(Page):
+class NewsPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    comment = RichTextField(blank=True)
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
+        index.SearchField('comment'),
     ]
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
+        FieldPanel('comment', classname="full"),
     ]
 
+class NewsIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
